68d74792254227f3511ca63173f60d921b447465
Robin <robin.miklinski@huddle.net>
2015-10-29 21:13:08 +0000

    test.py

--- python/api/test.py
+++ python/api/test.py
@@ -5,16 +5,28 @@ from pprint import pprint
 from zapv2 import ZAPv2, ZapError
 
 target = 'https://login.huddle.dev/'
-# zap = ZAPv2()
-# if ZAP is not listening on 8090
 zap = ZAPv2(proxies={'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'})
 
 print('Accessing target %s' % target)
 
-zap.urlopen(target)
-# Give the sites tree a chance to get updated
-time.sleep(2)
+try:
+	contextId = zap.context.import_context('HuddleContext.context')
+	pprint(zap.context.context_list)
+	pprint(zap.context.context('HuddleContext'))
+	zap.context.set_context_in_scope('HuddleContext', 'TRUE')
+except ZapError, e:
+	print(e.message)
 
+zap.context.include_in_context('HuddleContext', "\\Qhttps://login.huddle.dev\\E.*")
+
+# zap.authentication.set_login_url(1, 'https://login.huddle.dev/', postdata='username=Robin.Wmgr1&password=Steria12345$')
+# zap.authentication.set_login_indicator(1, 'you are logged in')
+# zap.authentication.login(1)
+
+# userId = zap.users.new_user(contextId, "Wmgr1")
+# print "userID = " + userId
+
+zap.urlopen(target)
 print('Spidering target %s' % target)
 zap.spider.scan(target)
 time.sleep(2)
@@ -41,8 +53,6 @@ except ZapError, e:
 	print(e.message)
 
 print('Scan completed')
-
-# Report the results
 
 print("Hosts: " + ", ".join(zap.core.hosts))
 print ('Alerts: ')
