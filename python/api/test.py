a85ec6ba033c75c2e1e7e541ba2d85a1939c800b
Robin <robin.miklinski@huddle.net>
2015-11-04 18:04:24 +0000

    Subprocess / set context scope

--- python/api/test.py
+++ python/api/test.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 
 import time
+import subprocess
 from pprint import pprint
 from zapv2 import ZAPv2, ZapError
 
 target = 'https://login.huddle.dev/'
 zap = ZAPv2(proxies={'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'})
 
+subprocess.Popen(['/path/to/zap.sh', '-daemon'], stdout=open(os.devnull, 'w'))
+time.sleep(10)
+
 print('Accessing target %s' % target)
 
-try:
-	contextId = zap.context.import_context('HuddleContext.context')
-	pprint(zap.context.context_list)
-	pprint(zap.context.context('HuddleContext'))
-	zap.context.set_context_in_scope('HuddleContext', 'TRUE')
-except ZapError, e:
-	print(e.message)
+
+HuddleContext = zap.context.import_context('D:\dev\ZAPClient\Contexts\HuddleContext.context')
+zap.context.set_context_in_scope(HuddleContext, True)
+pprint(zap.context.context_list)
+pprint(zap.context.context('HuddleContext'))
+zap.context.set_context_in_scope('HuddleContext', 'TRUE')
+
 
 zap.context.include_in_context('HuddleContext', "\\Qhttps://login.huddle.dev\\E.*")
+print('stop')
 
 # zap.authentication.set_login_url(1, 'https://login.huddle.dev/', postdata='username=Robin.Wmgr1&password=Steria12345$')
 # zap.authentication.set_login_indicator(1, 'you are logged in')
