03acc0af379fa54b54452c15b2eec12825db4008
Robin <robin.miklinski@huddle.net>
2015-12-29 18:29:33 +0000

    Refactor and settings for spider

---
+++ python/api/config.py
@@ -0,0 +1,11 @@
+Settings = dict(
+		userid="Robin.r89",
+		disk='C',
+		loginUri='https://login.huddle.test/',
+		myHuddleUri='https://my.huddle.test/',
+		contextName='HuddleContext_dev_staging',
+		contextFileName='HuddleContext_dev_staging.context',
+		ZAP_PROXY_HOST='127.0.0.1',
+		ZAP_PROXY_PORT=8080,
+		driver='${{Huddle-Selenium-Framework.SeleniumDriver}}'
+)
